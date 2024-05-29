09671e7f2a802a6c0863c23e63f4708cf5db8e46
unknown <you@example.com>
2024-05-29 10:45:42 -0300

    attqtsreactnavigation

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, StyleSheet, View, Image,FlatList} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Screens/Home';
+
+export default function App() {
+
+  const stack =  createStackNavigator  ();
+  return (
+    <NavigationContainer>
+ <stack.Navigator>
+
+  <stack.Screen name='Home' component={Home}></stack.Screen>
+
+ </stack.Navigator>
+    </NavigationContainer>
+
+      )}
+      
+       
+
+
+
+
+
+
+  
+      
+     
+      
+  
+ 
+
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#CAF0F8',
+    borderColor: '#90E0EF',
+    paddingHorizontal: 20,
+  }
+});
